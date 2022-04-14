cd29a505da9a06e90c3552b3ea62b8c49b0ed48d
noahkochanski <101604037+noahkochanski@users.noreply.github.com>
2022-04-14 16:24:52 -0400

    Added correlation plot

--- 2_eda_vis.R
+++ 2_eda_vis.R
@@ -35,3 +35,22 @@ ggplot(df.m, aes(year, value)) +
   geom_line() + 
   facet_wrap(~variable, scales = "free", ncol = 5) +
   theme_custom()
+
+
+
+
+# Correlation plot
+
+library(corrplot)
+library(RColorBrewer)
+library(ggpubr)
+
+
+M <- na.omit(us_df[,-1])
+colnames(M) <- c("C02", "Population", "GDP", "GDP Growth", "GDI", "Income", "Urban", "Energy", "Energy Import", "Electricity")
+M <-cor(M)
+
+corrplot(M, order = "hclust", type = "lower") + title("Correlation Plot for C02 Emissions Data")
+
+
+
